44073011acaf85e2c0fb892bd7285bd9afc52ce5
ArthurOrlov <81583856+ArthurOrlov@users.noreply.github.com>
2024-09-20 16:46:39 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var myFullpage = new fullpage("#fullpage", {
+    anchors: ["firstPage", "secondPage", "3rdPage"],
+  
+    navigationTooltips: ["First Section", "Second Section", "Third Section"],
+    navigation: true,
+  
+    slidesNavigation: true,
+  
+    controlArrows: true,
+  
+    licenseKey: "xxxxxxxxxxxxxxxxxxxxxxxxx"
+  });
+  
